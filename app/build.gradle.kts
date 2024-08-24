9352b2ee57ea554fc0f825270f2ff34ef0866680
kurokami-software <kurokami.software@gmail.com>
2024-08-24 15:49:02 -0400

    Implemented Splash Screen and Custom Icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    // implementation(libs.core.splashscreen)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
